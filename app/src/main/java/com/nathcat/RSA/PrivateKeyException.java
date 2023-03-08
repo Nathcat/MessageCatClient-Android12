f662b9768918c5d4c4759b25c8085bc1301b81ce
Nathcat <74734054+Nathcat@users.noreply.github.com>
2023-03-08 23:15:39 +0000

    Implemented libs for server classes and RSA classes

--- app/src/main/java/com/nathcat/RSA/PrivateKeyException.java
+++
@@ -1,7 +0,0 @@
-package com.nathcat.RSA;
-
-public class PrivateKeyException extends Exception {
-    public String toString() {
-        return "Private key is null!";
-    }
-}
--- app/src/main/java/com/nathcat/RSA/PublicKeyException.java
+++
@@ -1,7 +0,0 @@
-package com.nathcat.RSA;
-
-public class PublicKeyException extends Exception {
-    public String toString() {
-        return "Public key is null!";
-    }
-}
